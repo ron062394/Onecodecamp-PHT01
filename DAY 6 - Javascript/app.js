38dd9ba23038b9be253a043b004e04c496dd6fdd
ron062394 <ron062394@gmail.com>
2024-03-27 19:42:31 +0800

    Added day 6 Functions

---
+++ DAY 6 - Javascript/app.js
@@ -0,0 +1,25 @@
+//Functions - Allows us to write reusable blocks of code.
+
+//Defining a function
+function greet() {
+    console.log("Hello World!");
+    console.log("Hello Philippines!");
+}
+
+//Calling a function
+greet();
+
+//Function with Parameter
+function greetMe(name) {
+    console.log('Hello, ', name);
+}
+greetMe('Ralph');//Argument
+
+
+//Function with return
+function sum(a, b) {
+    console.log("Log me please.")
+    return a + b;
+    console.log("Log me again please.")
+}
+console.log('Sum', sum(5, 8));
